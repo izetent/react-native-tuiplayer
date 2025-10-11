4cc9aa587459422453b79c580342ebc9ac0b89d9
izetent <izetent@gmail.com>
2025-10-12 00:56:51 +0800

    feat: init

---
+++ ios/Tuiplayer.h
@@ -0,0 +1,5 @@
+#import <TuiplayerSpec/TuiplayerSpec.h>
+
+@interface Tuiplayer : NSObject <NativeTuiplayerSpec>
+
+@end
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import Tuiplayer from './NativeTuiplayer';
+
+export function multiply(a: number, b: number): number {
+  return Tuiplayer.multiply(a, b);
+}
